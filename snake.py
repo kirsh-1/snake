36739047fc9fc2919301447027e3420a1540d24a
kirsh-1 <157482278+kirsh-1@users.noreply.github.com>
2024-02-02 11:56:52 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -11,7 +11,7 @@ blue = (50, 153, 213)
 dis_width = 800
 dis_height = 600
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Змейка от Skillbox')
+pygame.display.set_caption('Змейка для 9 класса')
 clock = pygame.time.Clock()
 snake_block = 10
 snake_speed = 15
